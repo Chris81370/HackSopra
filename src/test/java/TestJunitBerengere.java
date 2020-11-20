c5ca6cb212b3fce74b4d825abce398dc654c6de7
Chris81370 <christophe.molinier@cimpa.com>
2020-11-20 12:36:44 +0100

    first commit

---
+++ src/test/java/TestJunitBerengere.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestJunitBerengere {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/TestJunitChristophe.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestJunitChristophe {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
