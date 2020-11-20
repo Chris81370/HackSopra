1119e5e4eec58ec47e8232be1ec8e93df4ab4154
Chris81370 <christophe.molinier@cimpa.com>
2020-11-20 15:08:25 +0100

    modification TestJunitChristophe

--- src/test/java/TestJunitChristophe.java
+++ src/test/java/TestJunitChristophe.java
@@ -3,11 +3,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pack.Abonne;
+import pack.Admin;
+import pack.ContreMesure;
+import pack.ServletAbonne;
+import pack.ServletAdmin;
+import pack.ServletContreMesure;
+import pack.ServletDeconnexion;
+import pack.ServletVulnerabilite;
+import pack.Vulnerabilite;
+
 class TestJunitChristophe {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void initialistationVaraibles() {
+
+		Abonne a = new Abonne();
+		Abonne a1 = new Abonne("nom", "prenom", "email");
+		Abonne am = new Abonne("email");
+		ContreMesure c = new ContreMesure();
+		ContreMesure c1 = new ContreMesure("description", "sources");
+		ContreMesure c2 = new ContreMesure(1, "titre", "description", "sources");
+
+		Vulnerabilite v = new Vulnerabilite();
+		Vulnerabilite v1 = new Vulnerabilite(1, "titre", "synthese", "description", "logiciels");
+		Admin ad = new Admin();
+		Admin ad1 = new Admin("ter", "ted");
+		Admin ad2 = new Admin("1", "identifiant", "mdp");
+
+		ServletAbonne sa = new ServletAbonne();
+		ServletAdmin sad = new ServletAdmin();
+		ServletContreMesure scm = new ServletContreMesure();
+		ServletDeconnexion sd = new ServletDeconnexion();
+		ServletVulnerabilite sv = new ServletVulnerabilite();
 	}
 
 }
